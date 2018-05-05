70b1b5b9cd3cdb3896d54a64dbac9b6054aa936e
mrtender <39006441+mrtender@users.noreply.github.com>
2018-05-05 16:24:31 +0100

    Done README
    This was a easy ride that looked very diffcult at the beginning.
    Big Thanks to Mark you the REAL MVPs

---
+++ tender.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+h1 {
+    color: #F2994A;
+    font-family: verdana;
+    font-size: 500%;
+    text-align: center;
+}
+h2 {
+    color: #F2994A;
+    font-family: verdana;
+    font-size: 300%;
+    text-align: center;
+}
+</style>
+</head>
+<body style="background-image:url('clouds.jpg')">
+</h2>
+<h1>HNG INTERNSHIP STAGE 1 TASK</h1>
+<h2>
+<?php
+ echo "DATE"  . date (" D-m-Y") . "<br>";
+?>
+</h2>
+<p>
+<h2>
+</h2>
+</p>
+<h2>
+<?php
+echo "TIME " . date("h:i:s a");
+?>
+</h2>
+</p>
+
+</body>
+</html>
